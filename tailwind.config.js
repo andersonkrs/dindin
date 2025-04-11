4890a1c14150915d64247d7accc6eb4a8837fe21
Anderson Macedo <anderson.krs95@gmail.com>
2025-04-11 07:28:46 -0300

    Tailwind upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,4 @@ module.exports = {
     "./app/assets/stylesheets/**/*.css",
     "./app/javascript/**/*.js",
   ],
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["emerald"],
-  },
-  theme: {
-    extend: {
-      strokeWidth: {
-        3: "3px",
-        4: "4px",
-        5: "5px",
-        6: "6px",
-        7: "7px",
-        8: "8px",
-      },
-    },
-  },
 };
