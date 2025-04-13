4f2e0c1e9e630b831e3c4977fc068476415d15ee
Anderson Macedo <anderson.krs95@gmail.com>
2025-04-13 11:02:40 -0300

    Move seeds to the onboarding command

--- app/models/onboarding.rb
+++ app/models/onboarding.rb
@@ -11,14 +11,16 @@ class Onboarding
       Category.expense.where(title: "Eating out").first_or_create!(icon: "restaurant", color: "rose")
       Category.expense.where(title: "Fitness").first_or_create!(icon: "fitness", color: "sky")
       Category.expense.where(title: "Groceries").first_or_create!(icon: "shopping-cart", color: "yellow")
-      Category.expense.where(title: "Heath").first_or_create!(icon: "hearth", color: "medium-red")
+      Category.expense.where(title: "Health").first_or_create!(icon: "heart", color: "medium-red")
       Category.expense.where(title: "Housing").first_or_create!(icon: "home-modern", color: "light-blue")
       Category.expense.where(title: "Leisure").first_or_create!(icon: "ticket", color: "fuchsia")
       Category.expense.where(title: "Others").first_or_create!(icon: "help", color: "dark-gray")
-      Category.expense.where(title: "Pets").first_or_create!(icon: "pawn", color: "dark-green")
+      Category.expense.where(title: "Pets").first_or_create!(icon: "paw", color: "dark-green")
       Category.expense.where(title: "Subscriptions").first_or_create!(icon: "credit-card", color: "dark-blue")
       Category.expense.where(title: "Shopping").first_or_create!(icon: "pricetags", color: "indigo")
       Category.expense.where(title: "Transportation").first_or_create!(icon: "car-sport", color: "lime")
+
+      Category.income.where(title: "Salary").first_or_create!(icon: "banknotes", color: "amber")
     end
 
     def setup_default_accounts!
